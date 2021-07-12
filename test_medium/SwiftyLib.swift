c326d76bd768e228991a4eea3c8930673e4f24fe
Camilo Lillo <camilolillo@MacBook-Pro-de-Camilo.local>
2021-07-12 17:17:24 -0400

    Added test cases

---
+++ test_medium/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  test_medium
+//
+//  Created by Camilo Lillo on 12-07-21.
+//
+//  SwiftyLib.swift
+//
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
