64c873ccaa998c1f3fc55aca4db441be805e938e
Camilo Lillo <camilolillo@MacBook-Pro-de-Camilo.local>
2021-07-12 15:56:49 -0400

    Initial project setup

---
+++ test_medium/test_medium.h
@@ -0,0 +1,18 @@
+//
+//  test_medium.h
+//  test_medium
+//
+//  Created by Camilo Lillo on 12-07-21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test_medium.
+FOUNDATION_EXPORT double test_mediumVersionNumber;
+
+//! Project version string for test_medium.
+FOUNDATION_EXPORT const unsigned char test_mediumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_medium/PublicHeader.h>
+
+
